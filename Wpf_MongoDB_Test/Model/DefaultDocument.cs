f53b6d3664a8459cb721ce41ac22018eb61fced2
htuszynski <hubertt83@gmail.com>
2017-09-28 03:36:47 +0200

    Initial commit + BsonElementAttribute

---
+++ Wpf_MongoDB_Test/Model/DefaultDocument.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Wpf_MongoDB_Test.Model
+{
+    public class DefaultDocument
+    {
+        [BsonElement("_id")]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("surname")]
+        public string Surname { get; set; }
+
+    }
+}
---
+++ Wpf_MongoDB_Test/ViewModel/MainViewModel.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Wpf_MongoDB_Test.Model;
+
+namespace Wpf_MongoDB_Test.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        protected static IMongoClient _client;
+        protected static IMongoDatabase _db;
+
+        public ObservableCollection<DefaultDocument> Docs { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+
+            _client = new MongoClient();
+            _db = _client.GetDatabase("est");
+            InitCollections();
+            FetchData();
+
+        }
+
+        private void InitCollections()
+        {
+            Docs = new ObservableCollection<DefaultDocument>();
+        }
+
+        private async void FetchData()
+        {
+            var collection = _db.GetCollection<BsonDocument>("default");
+            var filter = new BsonDocument();
+            using (var cursor = await collection.FindAsync(filter))
+            {
+                while (await cursor.MoveNextAsync())
+                {
+                    var batch = cursor.Current;
+                    foreach (var item in batch)
+                    {
+                        var ddoc = new DefaultDocument()
+                        {
+                            Id = item.GetElement("_id").Value.AsObjectId.ToString(),
+                            Name = item.GetElement("name").Value.AsString,
+                            Surname = item.GetElement("surname").Value.AsString
+                        };
+                        Docs.Add(ddoc);
+                    }
+                }
+            }
+        }
+    }
+}
